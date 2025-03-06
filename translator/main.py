6624967c82b2c37b34df7ed6bec2215db39cfc2c
wasvlad <vladuslav.nechiporuk@gmail.com>
2025-03-06 22:59:20 +0100

    main.py added endpoint to send translations words from en to ua. docker-compose.override.yml changed to expose translator api. Updated README.md. Told to ignore translator/data.

--- translator/main.py
+++ translator/main.py
@@ -1,3 +1,5 @@
+import json
+
 from fastapi import FastAPI
 import uvicorn
 
@@ -6,6 +8,10 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+@app.get("/en-to-ua-words")
+async def to_ua_words():
+    return json.loads(open("data/production/en_to_ua_words.json").read())
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=3002)
