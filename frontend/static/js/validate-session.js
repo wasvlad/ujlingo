029df93ac7069382efdd3eec6c05ebe2724c2e71
MSkrobot <Michal.skrobot@student.uj.edu.pl>
2025-03-31 03:39:51 +0200

    add link to redirect user to main page if he is logged in, and if he is not logged in previous links are shown

--- frontend/static/js/validate-session.js
+++ frontend/static/js/validate-session.js
@@ -1,23 +1,80 @@
 async function checkSession() {
+  const loginLink = document.getElementById('login-session-link');
+  const registerLink = document.getElementById('register-session-link');
+  const welcome = document.getElementById('welcome');
+
   try {
     const response = await fetch('/api/user/validate-session', {
       method: 'GET',
-      credentials: 'same-origin'
+      credentials: 'include'
     });
 
     if (response.ok) {
       const data = await response.json();
-      document.getElementById('welcome').innerText = data.message;
+
+      if (welcome) {
+        welcome.innerText = data.message;
+      }
+
+      if (loginLink) {
+        loginLink.innerHTML = `
+          You are already logged in
+          <a class="clickable-link" href="/html/main.html">Click here</a>
+        `;
+      }
+
+      if (registerLink) {
+        registerLink.innerHTML = `
+          You are already logged in
+          <a class="clickable-link" href="/html/main.html">Click here</a>
+        `;
+      }
+
     } else {
       const result = await response.json();
+
       if (result.detail === "Email is not confirmed") {
-        window.location.href = '/html/login.html?notice=unconfirmed';
-      } else {
-        window.location.href = '/html/login.html';
+          window.location.href = '/html/login.html?notice=unconfirmed';
+          return;
       }
-    }
+
+      if (loginLink || registerLink) {
+          if (loginLink) {
+          loginLink.innerHTML = `
+              Don't have an account?
+              <a class="clickable-link" href="/html/register.html">Create an account</a>
+          `;
+          }
+
+          if (registerLink) {
+          registerLink.innerHTML = `
+          Already have an account?
+              <a class="clickable-link" href="/html/login.html">Sign in</a>
+          `;
+          }
+  } else {
+    window.location.href = '/html/login.html';
+  }
+}
   } catch (error) {
     console.error('Error during session validation:', error);
-    window.location.href = '/html/login.html';
+
+    if (loginLink) {
+      loginLink.innerHTML = `
+        Don't have an account?
+        <a class="clickable-link" href="/html/register.html">Create an account</a>
+      `;
+    }
+
+    if (registerLink) {
+      registerLink.innerHTML = `
+        Already have an account?
+        <a class="clickable-link" href="/html/login.html">Sign in</a>
+      `;
+    }
+
+    if (!loginLink && !registerLink && !welcome) {
+      window.location.href = '/html/login.html';
+    }
   }
 }
